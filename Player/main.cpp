94ce95368a59e85080e7527fd8e7c5ed9836e464
Heo Jun-Yeong <shu04015@naver.com>
2020-07-23 12:06:30 +0900

    affine

--- Player/main.cpp
+++ Player/main.cpp
@@ -24,27 +24,85 @@
 #define ROTATE 'r'
 #define HIST 'h'
 #define MOVE 'm'
-#define SNAPSHOT 's'
+#define SIZE 's'
+#define FLIP 'f'
+#define SNAPSHOT 't'
 
 #define SHUTDOWN ESC
 
+typedef enum FLIPCODE
+{
+	NORMAL_FLIP = -2,
+	BILARTERAL_TOP_DOWN_FLIP = -1,
+	TOP_DOWN_FLIP = 0,
+	BILARTERAL_FLIP = 1
+}FLIPCODE;
 
 typedef struct size
 {
 	double col;
 	double row;
 }td_t;
+
 class ImageProcess
-{
+{ //default constructor
 public:
 	int bright = 0;
 	int contrast = 0;
 	int rotate = 0;
 	td_t move = { 0, 0 };
 	td_t size = { 0, 0 };
+	cv::Point2f inputQuad[4];
+	int flip = NORMAL_FLIP;
+
+	int adjust_enable = 0;
 };
 
 /********************************************/
+
+int setQuadPoint(cv::Point2f inputQuad)
+{
+	td_t point = { 0, 0 };
+	std::cout << "Call Set Quad mode" << std::endl;
+	std::cout << "UP : ROW + 10" << std::endl;
+	std::cout << "DOWN : ROW - 10" << std::endl;
+	std::cout << "RIGHT : COL + 1" << std::endl;
+	std::cout << "LEFT : COL - 1" << std::endl;
+
+	while (1)
+	{
+		char ch = 0;
+		char str[500];
+		switch (ch = cv::waitKey(0))
+		{
+		case UP:
+			point.row += 10;
+			break;
+		case DOWN:
+			point.row -= 10;
+			break;
+		case RIGHT:
+			point.col += 10;
+			break;
+		case LEFT:
+			point.col -= 10;
+			break;
+		case ESC:
+			inputQuad = cv::Point2f(point.row, point.col);
+			std::cout << "Quad [x : ";
+			std::cout << point.col << " y : " << point.row << std::endl;
+			std::cout << "Exit Quad Setting Mode~" << std::endl;
+			return 0;
+			break;
+		default:
+			break;
+		}
+		std::cout << "Quad [x : ";
+		std::cout << point.col << " y : " << point.row << "]" << std::endl;
+	}
+	return 0;
+}
+
 
 int setBright(cv::Mat inputImg, cv::Mat& outputImg)
 {
@@ -186,7 +244,7 @@ int setRotate(cv::Mat inputImg, cv::Mat& outputImg)
 	}
 	return rotate;
 }
-int setMove(cv::Mat inputImg, cv::Mat& outputImg, td_t &move)
+int setMove(cv::Mat inputImg, cv::Mat& outputImg, td_t& move)
 {
 	std::cout << "Call Set MOVE mode" << std::endl;
 	std::cout << "UP : ROW + 10" << std::endl;
@@ -254,7 +312,7 @@ int setSize(cv::Mat inputImg, cv::Mat& outputImg, td_t& size)
 			break;
 		case ESC:
 			std::cout << "Size [x : ";
-			std::cout << size.col << " y : " << size.row << std::endl;
+			std::cout << size.col << " y : " << size.row << "]" << std::endl;
 			std::cout << "Exit Size Mode~" << std::endl;
 			return 0;
 			break;
@@ -266,10 +324,83 @@ int setSize(cv::Mat inputImg, cv::Mat& outputImg, td_t& size)
 	}
 	return 0;
 }
-int setAdjust(cv::Mat& outputImg)
+int setFlip(cv::Mat inputImg, cv::Mat& outputImg)
 {
-	return 0;
+	static int flip = true;
+
+	std::cout << "Call Set Flip mode" << std::endl;
+	std::cout << "UP : top-down flip" << std::endl;
+	std::cout << "DOWN : bilateral flip" << std::endl;
+	std::cout << "LEFT or RIGHT : top-down and bilateral flip" << std::endl;
+
+	while (1)
+	{
+		char ch = 0;
+		switch (ch = cv::waitKey(0))
+		{
+		case UP:
+			std::cout << "top-down symmetry" << std::endl;
+			flip = TOP_DOWN_FLIP;
+			break;
+		case DOWN:
+			std::cout << "bilateral symmetry" << std::endl;
+			flip = BILARTERAL_FLIP;
+			break;
+		case LEFT:
+			std::cout << "normal" << std::endl;
+			flip = NORMAL_FLIP;
+			break;
+		case RIGHT:
+			std::cout << "top-down and bilateral symmetry" << std::endl;
+			flip = BILARTERAL_TOP_DOWN_FLIP;
+			break;
+		case ESC:
+			std::cout << "Flip = " << flip << std::endl;
+			std::cout << "Exit Flip Mode~" << std::endl;
+			return flip;
+		default:
+			break;
+		}
+		std::cout << "Flip = " << flip << std::endl;
+	}
 }
+int setQuad(cv::Mat& outputImg, cv::Point2f* inputQuad)
+{
+	
+	std::cout << "Call Set Quad mode" << std::endl;
+	std::cout << "Clockwise setting from top left" << std::endl;
+	std::cout << "UP : Top left" << std::endl;
+	std::cout << "DOWN : Top right" << std::endl;
+	std::cout << "RIGHT : Bottom right" << std::endl;
+	std::cout << "LEFT : Bottom left" << std::endl;
+
+	while (1)
+	{
+		char ch = 0;
+		switch (ch = cv::waitKey(0))
+		{
+		case UP:
+			setQuadPoint(inputQuad[0]);
+			break;
+		case DOWN:
+			setQuadPoint(inputQuad[1]);
+			break;
+		case RIGHT:
+			setQuadPoint(inputQuad[2]);
+			break;
+		case LEFT:
+			setQuadPoint(inputQuad[3]);
+			break;
+		case ESC:
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	return 1;
+}
+
 int main() {
 
 	cv::VideoCapture cap("test1.mp4");
@@ -312,13 +443,19 @@ int main() {
 			out.contrast = setContrast(inputImg, outputImg);
 			break;
 		case ADJUST:
-			setAdjust(inputImg);
+			out.adjust_enable = setQuad(inputImg, out.inputQuad);
 			break;
 		case ROTATE:
 			out.rotate = setRotate(inputImg, outputImg);
 			break;
 		case MOVE:
 			setMove(inputImg, outputImg, out.move);
+			break;
+		case SIZE:
+			setSize(inputImg, outputImg, out.size);
+			break;
+		case FLIP:
+			out.flip = setFlip(inputImg, outputImg);
 			break;
 		case SHUTDOWN:
 			std::cout << "bye~" << std::endl;
@@ -379,13 +516,28 @@ int main() {
 		}
 		if (out.move.col || out.move.row)
 		{
-			cv::Mat M = cv::Mat_<double>({ 2, 3 }, {1, 0, out.move.col, 0, 1, out.move.row});
+			cv::Mat M = cv::Mat_<double>({ 2, 3 }, { 1, 0, out.move.col, 0, 1, out.move.row });
 			cv::warpAffine(inputImg, outputImg, M, cv::Size());
 		}
 		if (out.size.col || out.size.row)
 		{
 			cv::resize(inputImg, outputImg, cv::Size(inputImg.cols + out.size.col, inputImg.rows + out.size.row), 0, 0, cv::INTER_NEAREST);
+		}
+		if (out.flip != NORMAL_FLIP)
+		{
+			cv::flip(inputImg, outputImg, out.flip);
+		}
+		if (out.adjust_enable)
+		{
+			cv::Point2f outputQuad[4];
 
+			outputQuad[0] = cv::Point2f(0, 0);
+			outputQuad[1] = cv::Point2f(inputImg.cols - 1, 0);
+			outputQuad[2] = cv::Point2f(inputImg.cols - 1, inputImg.rows - 1);
+			outputQuad[3] = cv::Point2f(0, inputImg.rows - 1);
+			
+			cv::Mat lambda = getPerspectiveTransform(out.inputQuad, outputQuad);
+			cv::warpPerspective(inputImg, outputImg, lambda, cv::Size());
 		}
 		std::cout << "b : " << out.bright << "c : " << out.contrast << "r : " << out.rotate 
 			<< "m [ x :  " << out.move.col << "  y : " << out.move.row <<" ]"<<std::endl;
